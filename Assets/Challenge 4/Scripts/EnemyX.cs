849e0de235b03f07f9f2790f44f7d0f85d4691f8
mervezgin <mervezgin@gmail.com>
2024-04-19 14:36:44 +0300

    initial commit
    project starter files

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    Rigidbody enemyRb;
+    GameObject player;
+
+    [SerializeField] float speed;
+
+    /*
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3
+        enemyRb.AddForce(player.transform.position - transform.position * speed);
+        /*
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+
+        */
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        /*
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+        */
+    }
+
+}
---
+++ Assets/Challenge 4/Scripts/RotateCameraX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCameraX : MonoBehaviour
+{
+    [SerializeField]float rotationspeed;
+
+    /*
+    public GameObject player;
+    */
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * rotationspeed * Time.deltaTime);
+        /*
+        transform.position = player.transform.position; // Move focal point with player
+        */
+    }
+}
